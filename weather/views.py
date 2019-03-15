b753446ec682424918de5a0c2bd2842588419f37
contail <qkrtkdwls2121@gmail.com>
2019-03-15 18:24:46 +0900

    add wether class

---
+++ weather/views.py
@@ -0,0 +1,15 @@
+import random
+from whale.settings import OPEN_DATA_API_KEY                 
+
+class Weather():
+    __API_BASE_URL = 'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName=%EC%A2%85%EB%A1%9C%EA%B5%AC&dataTerm=month&pageNo=1&numOfRows=10&ServiceKey=D1Is6KXEJ4hI1aeH9lFZSDZlH0Uc8saQeYBggMv50ug75pEABlirW9R%2F6rwgSMGBdHW0slD98BI6BnODE8RGkg%3D%3D&ver=1.3'
+    service_key = OPEN_DATA_API_KEY
+    
+    def __request():
+        url = ""
+
+    def __api_url_parms():
+        pass
+
+    def area_name():
+        pass
