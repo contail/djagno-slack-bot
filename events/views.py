4bbd4fdc4aa340793a8bfe9a08ee417a09491725
contail <qkrtkdwls2121@gmail.com>
2019-03-16 15:34:39 +0900

    add call order

--- events/views.py
+++ events/views.py
@@ -6,19 +6,20 @@ from slackclient import SlackClient
 import random
 from push.views import *
 from aws.views import *
+from weather.views import *
 SLACK_VERIFICATION_TOKEN = getattr(settings, 'SLACK_VERIFICATION_TOKEN', None)
 SLACK_BOT_USER_TOKEN = getattr(settings, 'SLACK_BOT_USER_TOKEN', None)                                     
 Client = SlackClient(SLACK_BOT_USER_TOKEN)                        
 
 class Events(APIView):
 
-    hello_list = ['안녕하세요~', '어서오세요~', '반가워요~']
+    hello_list = ['안녕하세요~ㅎㅎ', '반가워요~ㅎㅎ']
     goobye_list = ['잘가요 ㅠㅠ', '안녕히가세요 ㅜㅜ']
     luck_list = ['오늘은 운세가 좋아요!!', '오늘은 그럭저럭이네요~~', '오늘은 최악이에요 ㅜㅜ']
     introduce = """
     안녕하세요~ 제이름은 고래입니다 :whale:
     현재 가능한 명령어는
-    '운세', '푸시 정보', '푸시 결과', '웹서버 몇대야', '서버 상태' 입니다.
+    '운세', '푸시 정보', '푸시 결과', '웹서버 몇대야', '서버 상태', '미세먼지 [지역]' 입니다.
     지속적으로 업데이트 중입니다! 
     감사합니다~ 
     """
@@ -26,14 +27,12 @@ class Events(APIView):
     def post(self, request, *args, **kwargs):
          
         slack_message = request.data
-        
         if slack_message.get('token') != SLACK_VERIFICATION_TOKEN:
             return Response(status=status.HTTP_403_FORBIDDEN)
         
         if slack_message.get('type') == 'url_verification':
             return Response(data=slack_message, status=status.HTTP_200_OK)
 
-        print(slack_message,"======")
         # greet bot
         if 'event' in slack_message:                               
             event_message = slack_message.get('event')             
@@ -84,17 +83,31 @@ class Events(APIView):
                 elif '서버 상태' in text.lower():
                     aws = AWS()
                     payload = aws._get_server_status()
-                    print(payload)
-                
                     Client.api_call(method='chat.postMessage',         
                                     channel=channel,                   
                                     text="서버상태",
                                     attachments=payload['attachments'])  
-                    return Response(status=status.HTTP_200_OK)   
-                Client.api_call(method='chat.postMessage',         
-                                channel=channel,                   
-                                text=bot_text)                 
+                    return Response(status=status.HTTP_200_OK)  
+                elif '미세먼지' in text.lower():
+                    area_split = text.split("미세먼지")[1]
+                    ment = "해당 측정소를 찾지 못했어요~ 정확한 측정소는 아래 사이트에서 확인해주세요!\
+                                            http://www.airkorea.or.kr/web/stationInfo?pMENU_NO=93"
+                    bot_text = '<@{0}> {1}'.format(user, ment)
+                    if area_split is not None:
+                        weather = Weather()
+                        result, payload = weather.get_area_name(str(area_split).strip())
+                        if result:
+                            bot_text = '<@{0}>'.format(user)
+                            Client.api_call(method='chat.postMessage',
+                                            channel=channel,
+                                            text=bot_text,
+                                            attachments=payload['attachments'])
+                            return Response(status=status.HTTP_200_OK)
+                Client.api_call(method='chat.postMessage',
+                                channel=channel,
+                                text=bot_text)
             return Response(status=status.HTTP_200_OK)   
+
         if 'payload' in slack_message:
             if slack_message['payload'].get('attachments'):
                 print(slack_message['payload'])
