c2c44c72c0d835f18d7295b9ed1c016691f56697
contail <qkrtkdwls2121@gmail.com>
2019-03-28 02:29:10 +0900

    edit env

--- whale/secret_manager.py
+++ whale/secret_manager.py
@@ -10,10 +10,10 @@ def get_secret():
         session = boto3.session.Session()
         client = session.client(
             service_name='secretsmanager',
-            region_name=credentials['REGION_NAME'],
+            region_name=os.environ['REGION_NAME'],
             endpoint_url='https://secretsmanager.ap-northeast-2.amazonaws.com',
-            aws_access_key_id=credentials['aws_access_key_id'], aws_secret_access_key=credentials['aws_secret_access_key']
+            aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'], aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY']
         )
-        secret_value = client.get_secret_value(SecretId=credentials['SECRET_NAME'])
+        secret_value = client.get_secret_value(SecretId=os.environ['SECRET_NAME'])
         secret_value = json.loads(secret_value['SecretString'])
         return secret_value
