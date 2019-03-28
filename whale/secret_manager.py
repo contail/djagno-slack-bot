865a02700dadece382ced3561bb19d0b3fb89c49
contail <qkrtkdwls2121@gmail.com>
2019-03-29 02:18:54 +0900

    edit env var

--- whale/secret_manager.py
+++ whale/secret_manager.py
@@ -3,20 +3,21 @@ import json
 import os
 from botocore.exceptions import ClientError
 
-REGION_NAME = "ap-northeast-2"
+SECRET_NAME = os.environ["SECRET_NAME"]
+REGION_NAME = os.environ["REGION_NAME"]
 
 
 def get_secret():
-    dirname = os.path.dirname(__file__)
-    with open(os.path.join(dirname, 'credentials.json')) as f:
-        credentials = json.load(f)
-        session = boto3.session.Session()
-        client = session.client(
-            service_name='secretsmanager',
-            region_name=REGION_NAME,
-            endpoint_url='https://secretsmanager.ap-northeast-2.amazonaws.com',
-            aws_access_key_id=credentials['aws_access_key_id'], aws_secret_access_key=credentials['aws_secret_access_key']
-        )
-        secret_value = client.get_secret_value(SecretId=credentials['SECRET_NAME'])
-        secret_value = json.loads(secret_value['SecretString'])
-        return secret_value
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=REGION_NAME,
+        endpoint_url='https://secretsmanager.ap-northeast-2.amazonaws.com',
+        aws_access_key_id=os.environ['AWS_KEY'], aws_secret_access_key=os.environ['AWS_SECRET']
+    )
+    secret_value = client.get_secret_value(SecretId=SECRET_NAME)
+    secret_value = json.loads(secret_value['SecretString'])
+    return secret_value
+
+
+
